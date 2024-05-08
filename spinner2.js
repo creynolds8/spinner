5ed43817aecfca5b9a06606d78d6666cfd849ae1
creynolds8 <cameron8.cr@gmail.com>
2024-05-07 18:41:33 -0700

    add parameter to allow setting the speed of spinner, default 200

--- spinner2.js
+++ spinner2.js
@@ -1,10 +1,11 @@
-const spinner = function () {
-  const spinnerArr = ['\r|   ', '\r/   ', '\r-   ', '\r\\    ', '\r|   ', '\r/   ', '\r-   ', '\r\\    ', '\r|   ', '\n']
+const spinner = function(value) {
+  const delay = value || 200
+  const spinnerArr = ['\r|   ', '\r/   ', '\r-   ', '\r\\    ', '\r|   ', '\r/   ', '\r-   ', '\r\\    ', '\r|   ', '\n'];
   for (let i = 0; i < spinnerArr.length; i++) {
     setTimeout(() => {
-      process.stdout.write(spinnerArr[i])
-    }, i * 200) 
+      process.stdout.write(spinnerArr[i]);
+    }, i * delay);
   }
-}
+};
 
-spinner()
+spinner(500);
