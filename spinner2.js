7b88fc019905e7de1f7fcd7c56875efaf1f3717f
creynolds8 <cameron8.cr@gmail.com>
2024-05-07 18:28:50 -0700

    implement concise spinner logic

--- spinner2.js
+++ spinner2.js
@@ -1,35 +1,10 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
+const spinner = function () {
+  const spinnerArr = ['\r|   ', '\r/   ', '\r-   ', '\r\\    ', '\r|   ', '\r/   ', '\r-   ', '\r\\    ', '\r|   ', '\n']
+  for (let i = 0; i < spinnerArr.length; i++) {
+    setTimeout(() => {
+      process.stdout.write(spinnerArr[i])
+    }, i * 200) 
+  }
+}
 
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  process.stdout.write('\r\\    ');
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1100);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 1300);
-
-setTimeout(() => {
-  process.stdout.write('\r\\    ');
-}, 1500);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 1700);
+spinner()
